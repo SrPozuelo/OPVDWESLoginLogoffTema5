d567f18c6d6f73d91efff44ed727638c98956c96
SrPozuelo <oscar.pozvil@educa.jcyl.es>
2026-04-07 12:19:21 +0200

    Update scripts.

--- scripts/EDOPVCargaInicial.sql
+++ scripts/EDOPVCargaInicial.sql
@@ -1,6 +1,6 @@
 /**
  * Autor: Óscar Pozuelo
- * Createdo el 16 de marzo del 2026
+ * Createdo el 07 de abril del 2026
  * Script de carga inicial.
  */
 use DBOPVDWESProyectoTema4;
@@ -11,3 +11,19 @@ insert into T02_Departamento (T02_CodDepartamento,T02_DescDepartamento,T02_Fecha
     ('MAT','Departamento de matematicas.',now(),735.2,null),
     ('ING','Departamento de ingles.',now(),235.9,now()
 );
+INSERT INTO T01_Usuario (T01_CodUsuario,T01_Password,T01_DescUsuario)VALUES
+    ('vero',SHA2('veropaso',256),'Véro Grué'),
+    ('heraclio',SHA2('heracliopaso',256),'Heraclio Borbujo'),
+    ('alvaroA',SHA2('alvaroApaso',256),'Alvaro Allen'),
+    ('alejandro',SHA2('alejandropaso',256),'Alejandro De La Huerga'),
+    ('alvaroG',SHA2('alvaroGpaso',256),'Alvaro García'),
+    ('gonzalo',SHA2('gonzalopaso',256),'Gonzalo Junquera'),
+    ('cristian',SHA2('cristianpaso',256),'Cristian Mateos'),
+    ('alberto',SHA2('albertopaso',256),'Alberto Méndez'),
+    ('enrique',SHA2('enriquepaso',256),'Enrique Nieto'),
+    ('james',SHA2('jamespaso',256),'James Edward Nuñez'),
+    ('oscar',SHA2('oscarpaso',256),'Oscar Pozuelo'),
+    ('jesus',SHA2('jesuspaso',256),'Enrique Nieto'),
+    ('amor',SHA2('amorpaso',256),'Amor Rodriguez'),
+    ('albertoB',SHA2('albertoBpaso',256),'Alberto Bahillo')
+;
--- scripts/EDOPVCreacion.sql
+++ scripts/EDOPVCreacion.sql
@@ -1,17 +1,25 @@
 /**
  * Autor: Óscar Pozuelo
- * Createdo el 16 de marzo del 2026
+ * Createdo el 07 de abril del 2026
  * Script de creación de tablas y usuarios.
  */
-create database if not exists DBOPVDWESProyectoTema4;
-create table if not exists DBOPVDWESProyectoTema4.T02_Departamento(
+create database if not exists DBOPVDWESProyectoTema5;
+create table if not exists DBOPVDWESLoginLogoffTema5.T02_Departamento(
     T02_CodDepartamento varchar(3) primary key,
     T02_DescDepartamento varchar(255),
     T02_FechaCreacionDepartamento datetime not null,
     T02_VolumenDeNegocio float null,
     T02_FechaBajaDepartamento datetime null
 )engine=innodb;
-create user if not exists 'userOPVDWESProyectoTema4'@'%' identified by '5813Libro-Puro';
+create table if not exists DBOPVDWESLoginLogoffTema5.T01_Usuario(
+    T01_CodUsuario varchar(10) not null primary key,
+    T01_Password varchar(255) not null,
+    T01_NumConexiones int not null default 0,
+    T01_FechaHoraUltimaConexion datetime default null,
+    T01_Perfil varchar (100) not null default 'usuario',
+    T01_ImagenUsuario BLOB default null
+)engine==innodb;
+create user if not exists 'userOPVDWESLoginLogoffTema5'@'%' identified by '5813Libro-Puro';
 -- create user if not exists 'userOPVDWESProyectoTema4'@'%' identified by 'paso';
-grant all privileges on *.* to 'userOPVDWESProyectoTema4'@'%' with grant option;
+grant all privileges on *.* to 'userOPVDWESLoginLogoffTema5'@'%' with grant option;
 flush privileges;
